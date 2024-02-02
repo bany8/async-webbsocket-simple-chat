c89c5f2ff5d2f8753bafc8bd707fe1c3add86a61
bany <bart-7@outlook.com>
2024-02-02 20:20:30 +0100

    client side ready'

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
-import { createBoard, playMove } from "./connect4.js";
+function sendMessage(websocket) {
+  const button = document.getElementById("button");
+  button.addEventListener("click", () => {
+    const name = document.getElementById("name").value;
+    const message = document.getElementById("message").value;
+
+    const event = {
+      "type": "chat",
+      "name": name,
+      "message": message,
+    };
+    websocket.send(JSON.stringify(event));
+  });
+}
+
+function receiveMessage(websocket) {
+  websocket.addEventListener("message", ({ data }) => {
+    const event = JSON.parse(data);
+
+    const chatElement = document.getElementById("chat");
+    const pElement = document.createElement("p");
+    const spanElement = document.createElement("span");
+    
+    spanElement.textContent = event.time + " " + event.name + ": ";
+    pElement.textContent = event.message_text;
+    
+    pElement.insertBefore(spanElement, pElement.firstChild);
+    chatElement.append(pElement);
+  });
+}
 
 window.addEventListener("DOMContentLoaded", () => {
-  // Initialize the UI.
-  const board = document.querySelector(".board");
-  createBoard(board);
+  const websocket = new WebSocket("ws://localhost:7777/");
+  sendMessage(websocket);
+  receiveMessage(websocket);
 });
